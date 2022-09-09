b4bbd947d9cae1d3740a239d9d26bd090dfdf721
Gulshan Kumar <113016801+GulshanT113@users.noreply.github.com>
2022-09-09 22:58:41 +0530

    Time: 9 ms (70.04%), Space: 44.9 MB (37.23%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i : nums) hm.put(i, hm.getOrDefault(i, 0) + 1);
+        Pair[] p = new Pair[hm.size()];
+        int idx = 0;
+        for(int i : hm.keySet()) p[idx++] = new Pair(i, hm.get(i));// sir wala code h jisme humne khud ki pair
+        Arrays.sort(p);                                        // class banai thi for implimentation purpose.
+        int ans[] = new int[nums.length];
+        idx = 0;
+        for(Pair pairObj : p)  {
+            int e = pairObj.ele, f = pairObj.freq;
+            while(f-- > 0)
+                ans[idx++] = e;
+        }
+        return ans;
+    }
+    class Pair implements Comparable<Pair> {
+        int ele;
+        int freq;
+        Pair(int ele, int freq) {
+            this.ele = ele;
+            this.freq = freq;
+        } 
+        @Override
+        public int compareTo(Pair other) {
+            if(other.freq == this.freq) 
+                return other.ele - this.ele; 
+            return this.freq - other.freq;
+        }
+    }
+}
