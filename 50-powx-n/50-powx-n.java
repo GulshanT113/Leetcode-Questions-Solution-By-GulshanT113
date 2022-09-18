d4b26f08e76d43204e8dadd354ba3af4fa39caf7
Gulshan Kumar <113016801+GulshanT113@users.noreply.github.com>
2022-09-18 17:05:48 +0530

    Time: 1 ms (89.90%), Space: 41.7 MB (89.12%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        double ans=1.0;
+        Long power = new Long(n);
+        if(power<0){
+            x=1/x;
+            power = Math.abs(power);
+        }
+        while(power>0){
+            if((power & 1)==1){
+                ans *= x;
+            }
+            x *= x;
+            power = power >> 1;
+        }
+        return ans;
     }    
 }
