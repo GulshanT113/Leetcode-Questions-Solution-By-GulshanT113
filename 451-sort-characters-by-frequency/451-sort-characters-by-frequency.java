97cfc8270c16a687d7f93f09c4193048e6d74ecb
Gulshan Kumar <113016801+GulshanT113@users.noreply.github.com>
2022-09-23 19:26:01 +0530

    Time: 38 ms (38.17%), Space: 49 MB (61.11%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> hm = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) hm.put(c, hm.getOrDefault(c, 0) + 1);
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(char c : hm.keySet()) pq.add(new Pair(c, hm.get(c)));
+        while(!pq.isEmpty()) {
+            Pair rv = pq.remove();
+            for(int i = 0; i < rv.f; i ++) sb.append(rv.c);
+        }
+        return sb.toString();
+    }
+    class Pair implements Comparable<Pair>{
+        char c;
+        int f;
+        Pair(char c, int f) {
+            this.c = c; 
+            this.f = f;
+        }
+        
+        public int compareTo(Pair other) {
+            return other.f - this.f;
+        }
+    }
+}
