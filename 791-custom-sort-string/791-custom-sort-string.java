f78d85527df64953a66bdb836f5f178d71f9c02b
Gulshan Kumar <113016801+GulshanT113@users.noreply.github.com>
2022-09-24 18:41:05 +0530

    Time: 12 ms (12.33%), Space: 43.6 MB (7.12%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.java
+++ 791-custom-sort-string/791-custom-sort-string.java
@@ -2,16 +2,16 @@ class Solution {
     public String customSortString(String order, String s) {
         String s1 = "";
         String s2 = "";
-        int[] freq = new int[26];
-        for(char ch : order.toCharArray()) freq[ch-'a']++;
+        int[] freq = new int[26];// we declear the 26 size array for storing the charactors freq's.
+        for(char ch : order.toCharArray()) freq[ch-'a']++; // we store the order string freq here.
         for(char ch : s.toCharArray()) {
-            if(freq[ch-'a'] == 0) s2 += ch;
-            else freq[ch-'a']++;
+            if(freq[ch-'a'] == 0) s2 += ch; //we store in s2 only that perticuler charactor that not persent in order string.  
+            else freq[ch-'a']++; // and in this statement we store the freq of s string in freq array.
         }
         for(char ch : order.toCharArray()){
-            while(freq[ch-'a'] > 1){
+            while(freq[ch-'a'] > 1){ // we check here if freq is greater then 1 then we store the char in s1 string with respect to freq untill the freq equal to 1.
                 s1 += ch;
-                freq[ch-'a']--;
+                freq[ch-'a']--; // in this conditon we decrease the freq one by one after adding the one char in s1 string and one time we hit the while terminated condition with this process.
             }
         }
         return s1+s2;
