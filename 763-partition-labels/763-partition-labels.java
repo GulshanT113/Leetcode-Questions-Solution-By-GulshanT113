20b6dbf65b425ce999db7189f4faae6b8075f2be
Gulshan Kumar <113016801+GulshanT113@users.noreply.github.com>
2022-09-29 23:42:59 +0530

    Time: 9 ms (51.06%), Space: 42.7 MB (61.26%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,20 +1,15 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int n = s.length();
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for(int i = 0; i < n; i++) hm.put(s.charAt(i), i);
-        int currEnd = 0; //denotes the potential end of current partition
-        int currStart = 0;
-        List<Integer> ans = new ArrayList();
-        while(currEnd < n){
-            int i;
-            for (i = currStart; i <= currEnd; i++){
-                int newEnd = hm.get(s.charAt(i));
-                currEnd = Math.max(currEnd, newEnd);
+        int[] freq = new int[26];
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++) freq[s.charAt(i)-'a'] = i;
+        int max = 0, temp = -1;
+        for(int i = 0; i < s.length(); i++){
+            max = Math.max(max, freq[s.charAt(i)-'a']);
+            if(max == i){
+                ans.add(max-temp);
+                temp = max;
             }
-            ans.add(currEnd - currStart + 1);
-            currStart = i;
-            currEnd = i;
         }
         return ans;
     }
